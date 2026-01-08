6f7cd24210dbb8b4ab44939dbbd85fb5b953db1a
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2026-01-08 17:37:34 +0100

    Rename and add some things

--- vggsound/unfold_vggsound_dataset.py
+++ vggsound/unfold_vggsound_dataset.py
@@ -7,15 +7,18 @@ import os, shutil
 
 if __name__ == '__main__':
 
-    src_data_dir = '/data/upftfg27/lfranceschi/vggsound/total_video_frames'
-    dst_data_dir = '/data/upftfg27/lfranceschi/vggsound/frames'
+    src_data_dir = '/projects/imva/vggsound/train/total_video_frames'
+    dst_data_dir = '/home/lfranceschi/repos/ACL-SSL/vggsound/frames'
 
     os.makedirs(dst_data_dir, exist_ok=True)
 
     video_names = os.listdir(src_data_dir)
 
-    for v_name in video_names:
+    for i, v_name in enumerate(video_names):
         frames = os.listdir(os.path.join(src_data_dir, v_name))
         center_frame = frames[round(len(frames)/2)]
 
-        shutil.copy(os.path.join(src_data_dir, v_name, center_frame), os.path.join(dst_data_dir, v_name, '.jpg'))
+        if i%100 == 0:
+            print(f'Moving file {i}/{len(video_names)}')
+
+        shutil.copy(os.path.join(src_data_dir, v_name, center_frame), os.path.join(dst_data_dir, v_name + '.jpg'))
